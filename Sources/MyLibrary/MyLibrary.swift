232e295a17f642c00df5e50e09fba5cdb28d16b5
Daniyal <daniyalyousuf44@gmail.com>
2024-11-16 11:52:28 -0500

    Add files via upload

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class Math {
+    static func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+}
